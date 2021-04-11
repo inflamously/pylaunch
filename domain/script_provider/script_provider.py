0b63a8b242b6201a8166a1e82d3c5e399c6ec6ab
TheCharmingCthulhu <david.dembinski.1996@gmail.com>
2021-04-12 00:23:38 +0200

    feat(backend): added more unittests and implemented basic script_provider functions.

--- domain/script_provider/script_provider.py
+++ domain/script_provider/script_provider.py
@@ -2,13 +2,13 @@ import os
 from infrastructure.json_store import json_store
 
 
-def validate_paths():
-    return False
+def validate_paths(script_provider):
+    return "local-path" in script_provider and "sync-path" in script_provider
 
 
-def query_config():
-    pass
+def query_config(path):
+    return json_store.load_config(path)["script-provider"]
 
 
-def get_local_scripts():
-    pass
+def local_scripts(script_provider):
+    return os.listdir(script_provider["local-path"])
---
+++ domain/script_provider/script_provider_parser_spec.py
@@ -0,0 +1,13 @@
+import os
+import unittest
+from domain.script_provider import script_provider
+from infrastructure.json_store import json_store
+
+
+class ScriptProviderTest(unittest.TestCase):
+    def test_script_parsing(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
